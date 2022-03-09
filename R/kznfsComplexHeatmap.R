4d4c33ecb212c5e03670a6d2554573e22c7dbdac
ferygood <yaochung41@gmail.com>
2022-03-09 16:55:07 +0100

    add description with cell size, save_pdf option

--- R/kznfsComplexHeatmap.R
+++ R/kznfsComplexHeatmap.R
@@ -1,14 +1,20 @@
 #' Use ComplexHeatmap to visualize KZNFs data from TEKRABber result
-#'
-#' @param df a TEKRABber geneDESeq2Log2 table
-#' @param top a numeric value to show top numer of genes in heatmap
+#' @description This function use ComplexHeatmap::Heatmap() to visualize KZNFs
+#' data generated from using TEKRABber. First it will load the built-in
+#' hmKZNFs337 and filter the dataframe. The dataframe is the file with
+#' extention geneDESeq2norm.csv from TEKRABber. You can specified how many of
+#' the KZNFs you want to visualize. The default is showing the top 40 KZNFs
+#' with the highest row means among all samples.
+#' @param df a TEKRABber geneDESeq2norm table
+#' @param top a numeric value to show top number of genes in heatmap
 #' @param species indicate the species name to convert ID
+#' @param filename the name for saving pdf file (if specified)
 #'
-#' @return a ComplexHeatmap heatmap
+#' @return a ComplexHeatmap heatmap (Formal class Heatmap)
 #' @export
 #'
 #' @examples
-kznfsComplexHeatmap <- function(df, top=40, species = "hsapiens") {
+kznfsComplexHeatmap <- function(df, top=40, species = "hsapiens", filename=NULL) {
 
     # this is a prototype, parameters can be added later
     data(hmKZNFs337)
@@ -21,5 +27,23 @@ kznfsComplexHeatmap <- function(df, top=40, species = "hsapiens") {
     }
 
     mat_df <- ensIDtoName(df_KZNFs, species)
-    ComplexHeatmap::Heatmap(mat_df, name="Hello")
+
+    g <- ComplexHeatmap::Heatmap(
+        mat_df,
+        cluster_columns = FALSE,
+        name = "normalized",
+        width = ncol(mat_df) * unit(5, "mm"),
+        height = nrow(mat_df) * unit(5, "mm")
+    )
+
+    # save file in pdf format if filename is specified
+    if (!is.null(filename)) {
+        tidyHeatmap::save_pdf(
+            g,
+            filename = filename
+        )
+    }
+
+    g
+
 }
--- R/teComplexHeatmap.R
+++ R/teComplexHeatmap.R
@@ -1,14 +1,15 @@
-#' Title
+#' Use ComplexHeatmap to visualize TEs data from TEKRABber result
+#' @description
+#' @param df a TEKRABber teDESeq2norm table
+#' @param teFilter a list of transposable elements subsets
+#' @param top a numeric value to show top number of genes in heatmap
+#' @param filename the name for saving pdf file (if specified)
 #'
-#' @param df
-#' @param teFilter
-#' @param top
-#'
-#' @return
+#' @return a ComplexHeatmap heatmap (Formal class Heatmap)
 #' @export
 #'
 #' @examples
-teComplexHeatmap <- function(df, teFilter=NULL, top=40) {
+teComplexHeatmap <- function(df, teFilter=NULL, top=40, filename=NULL) {
 
     # this is a prototype, parameters can be added later
     if (!is.null(teFilter)) {
@@ -21,5 +22,21 @@ teComplexHeatmap <- function(df, teFilter=NULL, top=40) {
         df <- df[order(rowMeans(df), decreasing = TRUE), ][seq_len(top), ]
     }
 
-    ComplexHeatmap::Heatmap(df, name="Hello")
+    g <- ComplexHeatmap::Heatmap(
+        df,
+        name="normalized",
+        width = ncol(df) * unit(5, "mm"),
+        height = nrow(df) * unit(5, "mm")
+    )
+
+    if (!is.null(filename)) {
+
+        save_pdf(
+            g,
+            filename = filename
+        )
+    }
+
+    g
 }
+
